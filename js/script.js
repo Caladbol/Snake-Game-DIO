54774fbc8bde953c76383ce118daf317eef16fc3
Thaynã Ferreira de Sousa <t.f.sousa0@gmail.com>
2021-03-17 19:30:54 -0300

    Random food generator

--- js/script.js
+++ js/script.js
@@ -60,7 +60,15 @@ function Start() {
     if (direction == "up") snakeY -= box;
     if (direction == "down") snakeY += box;
 
-    snake.pop();
+    if (snakeX != food.x || snakeY != food.y) {
+        snake.pop();
+    } else {
+        food.x = Math.floor(Math.random() * 15 + 1) * box;
+        food.y = Math.floor(Math.random() * 15 +1) * box
+    }
+
+
+ 
 
     let newHead = {
         x: snakeX,
@@ -70,4 +78,4 @@ function Start() {
     snake.unshift(newHead);
 }
 
-let jogo = setInterval(Start, 500);
+let jogo = setInterval(Start, 80);
